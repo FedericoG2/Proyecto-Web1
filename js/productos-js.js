482968078a28daa8d0def2a340d9a5bfe9ca7c3c
Mauro Favre <mauro_favre@hotmail.com>
2025-06-27 20:05:45 -0300

    first commit

---
+++ js/productos-js.js
@@ -0,0 +1,47 @@
+export const productos = [
+    {
+        titulo: 'CASITAS PARA NIÑOS',
+        imagen: 'recursos/img/casita.png',
+        botonTexto: 'Consultar precio'
+    },
+    {
+        titulo: 'TRANQUERAS',
+        imagen: 'recursos/img/tranquera.png',
+        botonTexto: 'COMPRAR'
+    },
+    {
+        titulo: 'MESADAS DE 1" Y 2"',
+        imagen: 'recursos/img/mesada2.png',
+        botonTexto: 'Consultar precio'
+    },
+    {
+        titulo: 'SILLAS',
+        imagen: 'recursos/img/sillas.png',
+        botonTexto: 'Consultar precio'
+    },
+    {
+        titulo: 'TABLONES',
+        imagen: 'recursos/img/mesa.png',
+        botonTexto: 'Consultar precio'
+    },
+    {
+        titulo: 'MESADAS DE 1" Y 2"',
+        imagen: 'recursos/img/mesada2.png',
+        botonTexto: 'Consultar precio'
+    },
+    {
+        titulo: 'CASITAS PARA NIÑOS',
+        imagen: 'recursos/img/casita.png',
+        botonTexto: 'Consultar precio'
+    },
+    {
+        titulo: 'ORGANIZADOR',
+        imagen: 'recursos/img/org2.png',
+        botonTexto: 'Consultar precio'
+    },
+      {
+        titulo: 'TRANQUERAS',
+        imagen: 'recursos/img/tranquera.png',
+        botonTexto: 'COMPRAR'
+    }
+]
